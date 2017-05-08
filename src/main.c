ff9edf7c645185a34dbb6b007ce695289f8de3fc
Guilherme de Pinho Montemovo <guimontemovo@gmail.com>
2017-05-08 07:26:53 -0300

    Alteracoes insiguinificantes

--- src/main.c
+++ src/main.c
@@ -17,11 +17,16 @@ int main(int argc, char *argv[]){
 		destruirCompanhia(cs[i]);
 	free(cs);
 
-	// s = lerTodosTamReg(output,&n_regs);
-	// for (int i = 0; i < n_regs; ++i){
-	// 	imprimirCompanhia(s[i]);
-	// 	printf("--------------\n");
-	// }
+	s = lerTodosTamReg(output,&n_regs);
+	for (int i = 0; i < n_regs; ++i){
+		imprimirCompanhia(s[i]);
+		printf("--------------\n");
+	}
+	for(int i=0;i<n_regs;i++)
+		destruirCompanhia(s[i]);
+	free(s);
+
+
 
 	c = buscarNumRegTamReg(output,2);
 	if(c){
@@ -30,17 +35,16 @@ int main(int argc, char *argv[]){
 	}
 	else printf("Não Encontrado!\n");
 	
-	// int count;
-	// s = buscarCampoTamReg(output, NOME_SOCIAL, "521 PARTICIPAÇOES S.A. - EM LIQUIDAÇÃO EXTRAJUDICIAL",&count);
-	// printf("%d\n",count);
-	// for(int i=0;i<count;i++){
-	// 	printf("--------------------\n");
-	// 	imprimirCompanhia(s[i]);
-	// 	printf("--------------------\n");
-	// }
-	// for(int i=0;i<count;i++)
-	// 	destruirCompanhia(s[i]);
-	// free(s);
+	int count;
+	s = buscarCampoTamReg(output, NOME_SOCIAL, "521 PARTICIPAÇOES S.A. - EM LIQUIDAÇÃO EXTRAJUDICIAL",&count);
+	for(int i=0;i<count;i++){
+		printf("--------------------\n");
+		imprimirCompanhia(s[i]);
+		printf("--------------------\n");
+	}
+	for(int i=0;i<count;i++)
+		destruirCompanhia(s[i]);
+	free(s);
 
 
 	fclose(output);
